9cc009700f36cf979b606ae363329721c4e0f2a0
Adam Billett <adam@mvpsohio.com>
2023-12-05 10:56:52 -0500

    made an admin user, added photos to the projects, migrated it all. Did some manual testing to ensure all pages work

--- projects/models.py
+++ projects/models.py
@@ -7,3 +7,4 @@ class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     technology = models.CharField(max_length=20)
+    image = models.FileField(upload_to="project_images/", blank=True)
--- projects/views.py
+++ projects/views.py
@@ -6,12 +6,14 @@ from projects.models import Project
 from django.shortcuts import render
 from projects.models import Project
 
+
 def project_index(request):
     projects = Project.objects.all()
     context = {
         "projects": projects
     }
     return render(request, "projects/project_index.html", context)
+
 
 # projects/views.py
 
